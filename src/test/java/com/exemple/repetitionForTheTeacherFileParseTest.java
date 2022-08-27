6a0682a09da77139ed368b115ebb550917867d85
TimeQA <qaeducation2022@gmail.com>
2022-08-27 13:10:19 +0300

    added all files

---
+++ src/test/java/com/exemple/repetitionForTheTeacherFileParseTest.java
@@ -0,0 +1,11 @@
+package com.exemple;
+
+import org.junit.jupiter.api.Test;
+
+public class repetitionForTheTeacherFileParseTest {
+
+    @Test
+    void pdfTest() {
+
+    }
+}
---
+++ src/test/java/com/exemple/repetitionForTheTeacherSelenideTest.java
@@ -0,0 +1,39 @@
+package com.exemple;
+
+import com.codeborne.selenide.*;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FilterInputStream;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
+import static java.nio.charset.StandardCharsets.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class repetitionForTheTeacherSelenideTest {
+
+    @Test
+    void downloadTest() throws Exception {
+        Selenide.open("https://github.com/junit-team/junit5/blob/main/README.md");
+        File file = $("#raw-url").download();
+        try (InputStream is = new FileInputStream(file)) {
+            byte[] fileContent = is.readAllBytes();
+            assertThat(new String(fileContent, UTF_8)).contains("Contributions to JUnit 5");
+        }
+    }
+
+    @Test
+    void uploadTest() {
+        Selenide.open("https://fineuploader.com/demos.html");
+        $("input[type=\"file\"]").uploadFromClasspath("CryptoProPDF_UserGuide.pdf");
+        $$("div.qq-dialog-message-selector").
+                findBy(Condition.text("CryptoProPDF_UserGuide.pdf has an invalid extension. " +
+                        "Valid extension(s): jpeg, jpg, gif, png.")).
+                shouldBe(visible);
+    }
+}
